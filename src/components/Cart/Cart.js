ca68d94dcb9b69ff5af1cdbc7b247fa9fb3d2a3b
QuynhTram200822 <ttqtram.20it3@vku.udn.vn>
2024-10-29 08:33:15 +0700

    update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,36 +6,40 @@ import "./Cart.css";
 function ParentComponent() {
   const [labelText, setLabelText] = useState(0);
 
-  return (
-    <div className=" cart container d-flex justify-content-center align-items-center">
-      <Button
-        className="plus me-3"
-        onClick={() => setLabelText(labelText + 1)}
-        variant="primary"
-      >
-        +
-      </Button>
+  const handleButtonClick1 = () => {
+    setLabelText(labelText + 1);
+  };
 
-      <ChildComponent label={labelText} />
-      <Button
-        className="minus ms-3"
-        onClick={() => setLabelText(labelText - 1)}
-        variant="primary"
+  const handleButtonClick2 = () => {
+    setLabelText(labelText - 1);
+  };
+
+  return (
+    <div className=" cart container d-flex flex-column justify-content-center align-items-center">
+      <ChildComponent
+        onButtonClick1={handleButtonClick1}
+        onButtonClick2={handleButtonClick2}
       >
-        -
-      </Button>
+        <label>{labelText}</label>
+      </ChildComponent>
     </div>
   );
 }
 
 // Component Con
-function ChildComponent({ label }) {
-  return <GrandChildComponent label={label} />;
-}
+function ChildComponent({ onButtonClick1, onButtonClick2, children }) {
+  return (
+    <div className=" cart container d-flex justify-content-center align-items-center">
+      <Button className="plus me-3" onClick={onButtonClick1} variant="primary">
+        +
+      </Button>
+      {children}
 
-// Component Cháu
-function GrandChildComponent({ label }) {
-  return <label>{label}</label>;
+      <Button className="minus ms-3" onClick={onButtonClick2} variant="primary">
+        -
+      </Button>
+    </div>
+  );
 }
 
 export default ParentComponent;
