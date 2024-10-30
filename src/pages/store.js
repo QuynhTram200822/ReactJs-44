2ef50667eed66e96f2eb094ac7018eb2aa442364
QuynhTram200822 <ttqtram.20it3@vku.udn.vn>
2024-10-30 14:02:53 +0700

    update

--- src/pages/store.js
+++ src/pages/store.js
@@ -1,15 +1,16 @@
-// store.js
 import { createStore } from "redux";
 
 const initialState = {
-  isLogIn: false, // Trạng thái ban đầu
+  isLogIn: localStorage.getItem("login") === "true", // Trạng thái ban đầu
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "SUCCESS": // Khi hành động là SUCCESS
+      localStorage.setItem("login", "true");
       return { ...state, isLogIn: true };
     case "FAILURE": // Khi hành động là FAILURE
+      localStorage.setItem("login", "false");
       return { ...state, isLogIn: false };
     default:
       return state;
