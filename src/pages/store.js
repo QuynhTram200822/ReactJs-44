3596580df9c34a3908d69a2f9301c6c3860f6f11
QuynhTram200822 <ttqtram.20it3@vku.udn.vn>
2024-10-31 08:29:44 +0700

    update

--- src/pages/store.js
+++ src/pages/store.js
@@ -1,16 +1,17 @@
 import { createStore } from "redux";
+import Cookies from "js-cookie";
 
 const initialState = {
-  isLogIn: localStorage.getItem("login") === "true", // Trạng thái ban đầu
+  isLogIn: Cookies.get("login") === "true", // Trạng thái ban đầu
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "SUCCESS": // Khi hành động là SUCCESS
-      localStorage.setItem("login", "true");
+      Cookies.set("login", "true");
       return { ...state, isLogIn: true };
     case "FAILURE": // Khi hành động là FAILURE
-      localStorage.setItem("login", "false");
+      Cookies.set("login", "false");
       return { ...state, isLogIn: false };
     default:
       return state;
